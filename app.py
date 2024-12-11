d0bd04d6ad6f5638939dbad057430b49db4c23b3
sbaskey <43499610+sbaskey@users.noreply.github.com>
2024-12-11 12:01:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import io
 from tensorflow.keras.models import load_model
 import pandas as pd
 import streamlit as st
+from sklearn.metrics import mean_squared_error
 
 
 x=pd.read_csv('test.csv')
 st.write(x)
-model = load_model('new_model.h5')
+model = load_model('new_model.h5',custom_objects={'mse': mean_squared_error})
 
